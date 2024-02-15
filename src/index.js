169842f5af92629c7ea3c25ed9ab3b028ee92889
Rohit More <rohitbmore2002@gmail.com>
2024-02-15 15:37:59 +0530

    react app setup

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
 import reportWebVitals from './reportWebVitals';
+import { LoginComponent } from './components/login/login.component'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <LoginComponent />
   </React.StrictMode>
 );
 
