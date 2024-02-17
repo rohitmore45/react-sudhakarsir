41e622cdc6837b1e139d25fb5e4ba8d4a040dbcb
Rohit More <rohitbmore2002@gmail.com>
2024-02-17 12:40:24 +0530

    fakestore api fetching using jQuery $ajax({})

--- src/components/fakestore-api/fakestore.component.jsx
+++ src/components/fakestore-api/fakestore.component.jsx
@@ -1,18 +1,57 @@
 import { useEffect, useState } from "react";
 import "./fakestore.component.css";
+import $ from "jquery";
 
 export function FakeStoreComponent() {
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([{}]);
+  const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
 
+  function handleHomeClick() {
+    LoadProducts("https://fakestoreapi.com/products");
+  }
+  function GetCartCount() {
+    setCartCount(cartItems.length);
+  }
+
+  function handleAddToCart(e) {
+    // setCartItems([...cartItems, item]);
+    // GetCartCount();
+    if (cartItems.find((item) => item.id == e.target.id)) {
+      alert("Item already in cart..");
+    } else {
+      fetch(`https://fakestoreapi.com/products/${e.target.id}`)
+        .then((response) => response.json())
+        .then((data) => {
+          cartItems.push(data);
+          GetCartCount();
+          alert(`${data.title}\n Added to Cart`);
+        });
+    }
+  }
   function LoadCategories() {
-    fetch("https://fakestoreapi.com/products/categories")
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(categories);
-        data.unshift("all");
-        setCategories(data);
-      });
+    //fetching using jquery method ajax
+    $.ajax({
+      method: "get",
+      url: "https://fakestoreapi.com/products/categories",
+      success: (response) => {
+        response.unshift("all");
+        setCategories(response);
+        // console.log(response);
+      },
+      error: (response) => {
+        // console.log(response);
+      },
+    });
+
+    // fetch("https://fakestoreapi.com/products/categories")
+    //   .then((response) => response.json())
+    //   .then((data) => {
+    //     // console.log(categories);
+    //     data.unshift("all");
+    //     setCategories(data);
+    //   });
   }
 
   function LoadProducts(url) {
@@ -30,13 +69,14 @@ export function FakeStoreComponent() {
       LoadProducts("https://fakestoreapi.com/products");
     } else {
       LoadProducts(
-        `https://fakestoreapi.com/products/categories/${event.target.value}`
+        `https://fakestoreapi.com/products/category/${event.target.value}`
       );
     }
   }
   useEffect(() => {
     LoadCategories();
     LoadProducts("https://fakestoreapi.com/products");
+    // GetCartCount();
   }, []);
 
   return (
@@ -46,7 +86,11 @@ export function FakeStoreComponent() {
           <h2>FakeStore</h2>
         </div>
         <div className="d-flex gap-3">
-          <span>Home</span>
+          <span>
+            <button onClick={handleHomeClick} className="btn text-white">
+              Home
+            </button>
+          </span>
           <span>Electronics</span>
           <span>Jewelery</span>
           <span>Men's Fashion</span>
@@ -56,7 +100,56 @@ export function FakeStoreComponent() {
           <span className="bi bi-search"></span>
           <span className="bi bi-heart"></span>
           <span className="bi bi-person"></span>
-          <span className="bi bi-cart"></span>
+          <button
+            className="btn btn-light position-relative"
+            data-bs-target="#cart"
+            data-bs-toggle="modal"
+          >
+            <span className="bi bi-cart"></span>
+            <span className="badge bg-danger position-absolute rounded-circle">
+              {cartCount}
+            </span>
+          </button>
+          <div className="modal fade" id="cart">
+            <div className="modal-dialog">
+              <div className="modal-content">
+                <div className="modal-header">
+                  <h2 className="text-primary">Your Cart Items</h2>
+                  <button
+                    className="btn btn-close"
+                    data-bs-dismiss="modal"
+                  ></button>
+                </div>
+                <div className="modal-body">
+                  <table className="table table-hover">
+                    <thead>
+                      <tr>
+                        <th>Title</th>
+                        <th>Preview</th>
+                        <th>Price</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {cartItems.map((item) => (
+                        <tr key={item.id}>
+                          <td>{item.title}</td>
+                          <td>
+                            <img src={item.image} height={100} />
+                          </td>
+                          <td>{item.price}</td>
+                          <td>
+                            <button className="btn btn-danger btn-sm">
+                              <span className="bi bi-trash-fill"></span>
+                            </button>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </header>
 
@@ -78,7 +171,7 @@ export function FakeStoreComponent() {
 
         <main className="col-10 d-flex flex-wrap justify-content-evenly">
           {products.map((item) => (
-            <div className="card m-2 p-2 " key={item.id}>
+            <div className="card m-2 p-2" key={item.id}>
               <img
                 src={item.image}
                 height={150}
@@ -95,12 +188,16 @@ export function FakeStoreComponent() {
                   <dt>Rating:</dt>
                   <dd>
                     <span className="bi bi-star-fill text-success"></span>
-                    {/* {item.rating.rate} [{item.rating.count}] */}
+                    {/* {item.rating.rate} {item.rating.count} */}
                   </dd>
                 </dl>
               </div>
               <div className="card-footer">
-                <button className="btn btn-primary btn-sm w-100 ">
+                <button
+                  id={item.id}
+                  className="btn btn-primary btn-sm w-100"
+                  onClick={handleAddToCart}
+                >
                   <span className="bi bi-cart4"></span>Add To Cart
                 </button>
               </div>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import '../node_modules/bootstrap-icons/font/bootstrap-icons.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js'
 import reportWebVitals from './reportWebVitals';
 
 import { LoginComponent } from './components/login/login.component'
